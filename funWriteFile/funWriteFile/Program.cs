0bb8edf5b698dbedd4ac3b8c915bc3d2b13b35b4
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2025-03-28 12:40:46 +0100

    creazione funzione di check e scrittura file

---
+++ funWriteFile/funWriteFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+namespace funWriteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static bool checkFile()
+        {
+            if (File.Exists("C:\\Users\\alunno\\Downloads\\DatiCampionato.txt"))
+            {
+                return true;
+            }
+            return false;
+        }
+        static void writeFile(String testo)
+        {
+            if (!checkFile()) {
+                File.WriteAllText("C:\\Users\\alunno\\Downloads\\DatiCampionato.txt", testo);
+            }
+        }
+    }
+}
